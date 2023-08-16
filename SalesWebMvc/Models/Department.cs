112fd46a868b8738667ddf3ec23378ed1149a108
karinaesparza <karinaesparza296@gmail.com>
2023-08-16 16:47:33 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,34 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public ICollection<Seller> Sallers = new List<Seller>();
+
+        public Department()
+        {
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sallers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sallers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,
+        Builled = 1,
+        Canceled = 2
+    }
+}
