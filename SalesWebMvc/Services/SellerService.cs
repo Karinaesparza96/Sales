3a4ed22c5e69ad6c1b858a2daa4e137abe4992cf
karinaesparza <karinaesparza296@gmail.com>
2023-08-17 12:20:08 -0300

    SellerFormViewModel and Departament select component

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -24,7 +24,6 @@ namespace SalesWebMvc.Services
 
         public void Insert(Seller seller)
         {
-            seller.Department = _context.Department.First();
             _context.Add(seller);
             _context.SaveChanges();
         }
